ee3df337612f73423d8d9b0c2b6a355b07fccbce
laike9m <laike9m@gmail.com>
2013-12-19 18:31:08 +0800

    multi nodes draw pic

---
+++ clean_email.py
@@ -0,0 +1,21 @@
+'''
+把-1节点全部删掉,为聚类做准备
+'''
+
+def clean(filename):
+    try:
+        email = open(filename, 'rt')
+    except:
+        print('no such file')
+    
+    with open('cleaned_email.txt', 'wt') as ce:
+        for line in email:
+            fromNode, toNode = line.split() 
+            if int(toNode) > 0:
+                ce.write(fromNode + '\t' + toNode + '\n')
+    
+    email.close()
+
+
+if __name__ == '__main__':
+    clean(filename='new_Email-EuAll.txt')
